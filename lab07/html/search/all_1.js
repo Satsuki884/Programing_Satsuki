0bf75bc0e61bfe330add86a5993571dce025653b
Satsuki884 <stasyaklim884@gmail.com>
2020-11-24 13:21:10 +0200

    lab07

--- lab07/html/search/all_1.js
+++ lab07/html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['function',['function',['../ex3_8c.html#a77655d9aaa4a2acbfa2d9e66932487a5',1,'ex3.c']]]
+  ['function',['function',['../ex3_8c.html#a5acd274c0e6ac78f4e3a3445de02691b',1,'ex3.c']]]
 ];
