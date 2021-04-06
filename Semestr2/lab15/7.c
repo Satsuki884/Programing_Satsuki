a78d04c7d53d1456a033e6b771db3773e3b93d80
Satsuki884 <stasyaklim884@gmail.com>
2021-04-06 09:02:59 +0300

    lab15-17

--- Semestr2/lab15/7.c
+++ Semestr2/lab15/7.c
@@ -14,16 +14,41 @@ int main(){
             m++;
         }
     }
-    int j =0;
-    struct shoes array[m];
-    for(int i = 0; i < 4; i++ ){
-        if(arr[i].size_shoes.size >=39){
-            fill_array(&array[j], &arr[i]);
-            j++;
+    int n = 0;
+    for(int i = 0; i< 4; i++){
+        if((strcmp(arr[i].brand_model, "Nike"))==0 || (strcmp(arr[i].brand_model, "Puma"))==0 && arr[i].ortopedic){
+            n++;
         }
     }
-    testing_fill_array_а(array);
-    sort(array, m);
-    print_array(array, m);
-    test_sort(array, m);
+    char a;
+    int j =0;
+    struct shoes array[m];
+    struct shoes arra[n];
+    printf("Введіть 'a' для знаходження чоботів разміром більше 39 та сортування іх, та 'b' для знаходженния ортопедичних чоботів бренду Nike та Puma:\n");
+    scanf("%c", &a);
+    switch(a){
+        case 'a':
+            printf("%c\n", a);
+            for(int i = 0; i < 4; i++ ){
+                if(arr[i].size_shoes.size >=39){
+                    fill_array(&array[j], &arr[i]);
+                    j++;
+                }
+            }
+            sort(array, m);
+            print_array(array, m);
+            test_sort(array,m);
+            break;
+        case 'b' :
+            for(int i = 0; i < 4; i++ ){
+                if(((strcmp(arr[i].brand_model, "Nike"))==0 || (strcmp(arr[i].brand_model, "Puma"))==0) && arr[i].ortopedic){
+                    fill_array(&arra[j], &arr[i]);
+                    j++;
+                }
+            }
+            print_array(arra, n);
+            break;
+        default:
+            printf( "Неправильный ввод.\n" );
+    }
 }
